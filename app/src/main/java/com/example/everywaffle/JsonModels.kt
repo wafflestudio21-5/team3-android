076ec732c8ee1239e5db696bebc8cb44dc034b07
plgafhd <plgafhd@snu.ac.kr>
2024-01-05 21:28:36 +0900

    로그인 전 화면 + 화면 간 이동

--- app/src/main/java/com/example/everywaffle/JsonModels.kt
+++ app/src/main/java/com/example/everywaffle/JsonModels.kt
@@ -1,4 +1,32 @@
 package com.example.everywaffle
 
-class JsonModels {
-}
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SignupRequest(
+    @Json(name = "userName") val userName:String,
+    @Json(name = "password") val password:String,
+    @Json(name = "email") val email:String
+)
+
+@JsonClass(generateAdapter = true)
+data class SignupResponse(
+    @Json(name = "userName") val userName:String,
+    @Json(name = "email") val email:String,
+    @Json(name = "token") val token:String
+)
+
+@JsonClass(generateAdapter = true)
+data class SigninRequest(
+    @Json(name = "userName") val userName:String,
+    @Json(name = "password") val password:String
+)
+
+@JsonClass(generateAdapter = true)
+data class SigninResponse(
+    @Json(name = "userId") val userId:Int,
+    @Json(name = "userName") val userName:String,
+    @Json(name = "email") val email:String,
+    @Json(name = "token") val token:String
+)
