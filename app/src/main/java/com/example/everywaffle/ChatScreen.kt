c18494c56e6798060e72303d4508bf7246805159
plgafhd <plgafhd@snu.ac.kr>
2024-02-02 16:18:53 +0900

    쪽지 기능 완료

--- app/src/main/java/com/example/everywaffle/ChatScreen.kt
+++ app/src/main/java/com/example/everywaffle/ChatScreen.kt
@@ -1,5 +1,6 @@
 package com.example.everywaffle
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +14,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -28,6 +31,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -35,7 +39,9 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -63,99 +69,258 @@ import kotlinx.coroutines.launch
 
 @Composable
 
-fun ChatScreen(navController: NavController) {
+fun ChatScreen(navController: NavHostController) {
+    val mainViewModel = hiltViewModel<MainViewModel>()
     var isBoatSelected by remember { mutableStateOf(true) }
+    val sessions = remember { mutableStateListOf<SessionDetail>() }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.Absolute.Left
-        ) {
-            Text(
-                text = "랜덤쪽지  ",
-                color = if (isBoatSelected) Color.Black else Color.Gray,
-                fontWeight = if (isBoatSelected) FontWeight.Bold else FontWeight.Normal,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .clickable {
-                        isBoatSelected = true
-
-                    }
-            )
-            Text(
-                text = "쪽지",
-                color = if (isBoatSelected) Color.Gray else Color.Black,
-                fontWeight = if (isBoatSelected) FontWeight.Normal else FontWeight.Bold,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .clickable {
-                        isBoatSelected = false
-
-                    }
-            )
+    LaunchedEffect(Unit){
+        sessions.clear()
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = mainViewModel.getsessions()
+            if(result!=null) sessions.addAll(result)
         }
+    }
 
-        Spacer(modifier = Modifier.weight(1f))
-
-        if (isBoatSelected) {
-            BottomAppBar(
-                modifier = Modifier.padding(16.dp),
-                contentPadding = PaddingValues(0.dp),
-                containerColor = Color.Transparent
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(top = 18.dp, start = 18.dp),
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.randommessage),
-                        contentDescription = "랜덤쪽지 보내기",
-                        tint = Color.Unspecified,
-                        modifier = Modifier
-                            .size(200.dp)
-                            .clickable {
-                                navController.navigate("SendScreen")
-                            }
-                    )
+                Text(
+                    text = "랜덤쪽지  ",
+                    color = if (isBoatSelected) Color.Black else Color(0xFFD3D3D3),
+                    fontWeight = if (isBoatSelected) FontWeight.Bold else FontWeight.Normal,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .clickable {
+                            isBoatSelected = true
+
+                        }
+                )
+                Text(
+                    text = "쪽지",
+                    color = if (isBoatSelected) Color(0xFFD3D3D3) else Color.Black,
+                    fontWeight = if (isBoatSelected) FontWeight.Normal else FontWeight.Bold,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .clickable {
+                            isBoatSelected = false
+
+                        }
+                )
+            }
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(.1f)
+                    .background(Color.White)
+            ){
+                sessions.forEach{
+                    item{
+                        SessionPreview(it, navController)
+                    }
                 }
             }
+
+            LowBar(navController = navController, key = 4) // TODO : 위치 조정 필요
         }
-        if(!isBoatSelected){
-            BottomAppBar(
-                modifier = Modifier.padding(16.dp),
-                contentPadding = PaddingValues(0.dp),
-                containerColor = Color.Transparent
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+
+        Row(
+            verticalAlignment = Alignment.Bottom,
+            modifier = Modifier.background(Color.Transparent)
+        ){
+            if (isBoatSelected) {
+                BottomAppBar( // TODO: 이 bottomappbar로 인해 Lowbar가 클릭이 되지 않는 현상 수정 필요
+                    modifier = Modifier.padding(16.dp),
+                    contentPadding = PaddingValues(0.dp),
+                    containerColor = Color.Transparent
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.normalmessage),
-                        contentDescription = "쪽지 보내기",
-                        tint = Color.Unspecified,
-                        modifier = Modifier
-                            .size(200.dp)
-                            .clickable {
-                                navController.navigate("SendScreen")
-                            }
-                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.randommessage),
+                            contentDescription = "랜덤쪽지 보내기",
+                            tint = Color.Unspecified,
+                            modifier = Modifier
+                                .size(200.dp)
+                                .clickable {
+                                    navController.navigate("SendScreen/0/${MyApplication.prefs.getString("userid").toInt()}")
+                                }
+                        )
+                    }
                 }
             }
         }
     }
 }
 
+@Composable
+fun SessionPreview(
+    session:SessionDetail,
+    navController: NavHostController
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(horizontal = 18.dp)
+            .clickable {
+                navController.navigate("Message/${session.sessionId}")
+            } // TODO:
+    ){
+        Spacer(modifier = Modifier.height(11.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(text = "익명", fontSize = 13.sp, fontWeight = FontWeight.Bold, color = Color.Black)
+            Text(text = timetoprint(session.lastMessage.createdAt), fontSize = 12.sp, color = Color(0xFF979797))
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = session.lastMessage.content, fontSize = 13.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(13.dp))
+    }
+}
+
+@Composable
+fun MessageView(
+    sessionid:Int?,
+    navController: NavHostController
+){
+    val mainViewModel = hiltViewModel<MainViewModel>()
+    val messages = remember { mutableStateListOf<MessageDetail>() }
+
+    LaunchedEffect(Unit){
+        messages.clear()
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = mainViewModel.getmessages(sessionid!!)
+            if(result!=null) messages.addAll(result)
+        }
+    }
+
+    Surface(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(66.dp)
+                    .padding(horizontal = 18.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row(
+                    modifier = Modifier.width(100.dp),
+                    horizontalArrangement = Arrangement.Start
+                ){
+                    Icon(
+                        painter = painterResource(id = R.drawable.backarrow),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(11.dp)
+                            .height(18.dp)
+                            .clickable { navController.navigate("Chat") }
+                    )
+                }
+                Text(text = "익명", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Black)
+                Row(
+                    modifier = Modifier.width(100.dp),
+                    horizontalArrangement = Arrangement.End
+                ){
+                    Icon(
+                        painter = painterResource(id = R.drawable.messagesend),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(18.dp)
+                            .height(18.dp)
+                            .clickable {
+                                navController.navigate("SendScreen/${sessionid}/${MyApplication.prefs.getString("userid").toInt()}")
+                            }
+                    )
+                    Spacer(modifier = Modifier.width(40.dp))
+                    Icon(
+                        painter = painterResource(id = R.drawable.threedots),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .width(3.dp)
+                            .height(19.dp)
+                            .clickable {}
+                    )
+                }
+            }
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(.1f)
+                    .background(Color.White)
+            ){
+                messages.forEach{
+                    item{
+                        MessageContent(it)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MessageContent(
+    message:MessageDetail
+){
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(horizontal = 18.dp)
+    ){
+        Spacer(modifier = Modifier.height(12.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(fontSize = 13.sp, fontWeight = FontWeight.Bold,
+                text = if(message.senderId == MyApplication.prefs.getString("userid").toInt()) "보낸 쪽지" else "받은 쪽지",
+                color = if(message.senderId == MyApplication.prefs.getString("userid").toInt()) Color(0xFF05BCBC) else Color(0xFFFFD330))
+            Text(text = timetoprint(message.createdAt), fontSize = 12.sp, color = Color(0xFF979797))
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = message.content, fontSize = 13.sp, color = Color.Black)
+        Spacer(modifier = Modifier.height(13.dp))
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 
-fun SendScreen(navController: NavController) {
+fun SendScreen(
+    navController: NavController,
+    sessionid:Int?,
+    senderid:Int?
+) {
+    val mainViewModel = hiltViewModel<MainViewModel>()
     var message by remember { mutableStateOf("") }
 
     MaterialTheme {
@@ -176,7 +341,7 @@ fun SendScreen(navController: NavController) {
                         }
                     },
                     navigationIcon = {
-                        IconButton(onClick = {navController.navigate("ChatScreen")}) {
+                        IconButton(onClick = {navController.popBackStack()}) {
                             Icon(
                                 imageVector = Icons.Filled.Close,
                                 contentDescription = "닫기",
@@ -186,7 +351,13 @@ fun SendScreen(navController: NavController) {
                     },
                     actions = {
                         IconButton(
-                            onClick = {navController.navigate("ChatScreen")}
+                            onClick = {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    val result = mainViewModel.sendmessage(SendMessage(sessionId = sessionid!!, senderId = senderid!!, content = message))
+                                    if (result==null){} // TODO:
+                                    else{navController.popBackStack()}
+                                }
+                            }
                         ) {
                             Icon(
                                 painter = painterResource(id = R.drawable.send),
